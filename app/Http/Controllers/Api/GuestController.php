c719136ffd23c49441e47ff20123a0be6288acd0
LucaFaedda <lucakarl91@gmail.com>
2023-03-13 15:16:28 +0100

    completato api

--- app/Http/Controllers/Api/GuestController.php
+++ app/Http/Controllers/Api/GuestController.php
@@ -12,5 +12,35 @@ use Illuminate\Support\Facades\Validator;
 
 class GuestController extends Controller
 {
-    //
+    public function store(Request $request){
+        $form_data = $request->all(); // Qua andiamo a recuperare i dati del form fatti in front-end
+        
+        $validator = Validator::make($form_data, [     // quan andiamo a validarli
+            'name'  => 'required',
+            'surname' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'message' => 'required',
+        ]);
+
+        if($validator->fails()){  // se fallisce il validator vado negli errori e li mostro a schermo
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $newContact = new Guest(); // se va tutto bene vengono inviati nel DB
+        $newContact->fill($form_data);
+        $newContact->save();
+
+        Mail::to('info@boolpres.com')->send(new GuestContact($newContact));
+
+        return response()->json([
+            'success' => true
+        ]);
+
+
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\GuestController;
 use App\Http\Controllers\Api\ProjectController;
 
 /*
@@ -16,6 +17,7 @@ use App\Http\Controllers\Api\ProjectController;
 */
 Route::get('projects', [ProjectController::class, 'index']);
 Route::get('project/{slug}', [ProjectController::class, 'show']);
+Route::post('contacts', [GuestController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
